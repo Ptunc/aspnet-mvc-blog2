e4f8680124e9ba63d40d390afaddc696a9279da7
Ptunc <spolentunc@gmail.com>
2023-07-21 17:32:52 +0300

    gorev 6

---
+++ AspNetBlog/AspNetBlog/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetBlog.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult Login(string redirectUrl)
+		{
+			return View();
+		}
+	}
+}
---
+++ AspNetBlog/AspNetBlog/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetBlog.Controllers
+{
+	public class BlogController : Controller
+	{
+		public IActionResult Detail(int id)
+		{
+			return View();
+		}
+        public IActionResult Search(string query, int page)
+        {
+            return View();
+        }
+    }
+}
